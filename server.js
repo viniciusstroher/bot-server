cb8b2b8274dad543071c86142393327aa7322aa9
unknown <viniciusferreirawk@gmail.com>
2018-10-15 22:37:22 -0300

    fix

--- server.js
+++ server.js
@@ -7,36 +7,48 @@ var https  	   = require('https');
 var fs 	       = require('fs');
 var multer 	   = require('multer');
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './files-static/') //Destination folder
-  }
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+
+app.use(bodyParser.json())
+
+app.post('/contatos',function(req,res){
+	console.log(req.body);
 });
 
-upload 	   = multer({storage:storage});
+// var storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './files-static/') //Destination folder
+//   }
+// });
 
-app.post('/files',upload.single('image'),function(req,res,next){
-	if(!req.file){
-		res.json({"status":"arquivo_nao_enviado", 
-				  msg: "Envie um arquivo"});
-		res.end();
+// upload 	   = multer({storage:storage});
+// app.post('/files',function(){
 
-	}else{
+// });
+// app.post('/files',upload.single('image'),function(req,res,next){
+// 	if(!req.file){
+// 		res.json({"status":"arquivo_nao_enviado", 
+// 				  msg: "Envie um arquivo"});
+// 		res.end();
 
-		var apiError   = "arquivo_nao_processado";
-		var apiSucc    = "arquivo_processado";
-		var ip 		   = req.connection.localAddress;
-		console.log(req.connection);
-		var urlrecurso = "/files-static/"+req.file.filename;
+// 	}else{
+
+// 		var apiError   = "arquivo_nao_processado";
+// 		var apiSucc    = "arquivo_processado";
+// 		var ip 		   = req.connection.localAddress;
+// 		console.log(req.connection);
+// 		var urlrecurso = "/files-static/"+req.file.filename;
 		
 
-		res.json({"status":apiSucc, 
-				   msg: "Arquivo enviado.",
-				   msgKey:"ARQUIVO_ENVIADO", 
-				   url:urlrecurso});
+// 		res.json({"status":apiSucc, 
+// 				   msg: "Arquivo enviado.",
+// 				   msgKey:"ARQUIVO_ENVIADO", 
+// 				   url:urlrecurso});
 
-	}
-});
+// 	}
+// });
 
 http.createServer(function (req, res) {
     // res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
